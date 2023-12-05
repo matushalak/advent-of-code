ffe6ded2af866457b31c9e8c63a5cb7a8d336483
matushalak <matus.halak@gmail.com>
2023-12-05 12:44:29 +0100

    day5 PART 1 SOLVED!

--- 2023/DAY5/day5.py
+++ 2023/DAY5/day5.py
@@ -8,11 +8,13 @@ DAY5
 You give a seed a fertilizer
 --------
 P1 Impossible with the implemented solution because numbers are so big
+P2 Seed number ranges
+    What is the lowest location number that corresponds to any of the initial seed numbers (within initial seed ranges)?
 """
 import re
 from numpy import arange
 
-with open('day5_example.txt', 'r') as almanac:
+with open('day5_input.txt', 'r') as almanac:
     maps = dict()
     current_map = ""
     for line in almanac:
@@ -36,47 +38,62 @@ with open('day5_example.txt', 'r') as almanac:
                         ))
         pass
 
-def make_map_dict (current_dict_vals
+# Part 2
+maps['seed_ranges'] = []
+for i, s in enumerate(maps['seeds'][0]):
+    if i % 2 == 0:
+        maps['seed_ranges'].append((s,s+maps['seeds'][0][i+1]))
+
+# print(maps['seed_ranges'])
+#%%
+def make_map_dict (current_dict_vals:list
                    ) -> dict:
     # need to just check whether value larger or smaller than
     # source range = tuple, destination range = tuple
-    # loop through the tuples, if number >= than source[0] and < source[1]
-        # match it to destination by (number - source[0]) + destination[0]
+    
     cd = dict()
     for row in current_dict_vals:
-        destination_range = arange(row[0], row[0]+row[-1])
-        source_range = arange(row[1], row[1]+row[-1])
+        destination_range = (row[0], row[0]+row[-1])
+        source_range = (row[1], row[1]+row[-1])
+
+        # for big numbers        
+        cd[source_range] = destination_range
         
-        if cd == {}:
-            cd = {s:d for s,d in zip(source_range,destination_range)}
-        
-        else:
-            for s,d in zip(source_range,destination_range):
-                cd[s] = d
     return cd
         
         
 # Make big dictionary
 big_dict = dict()
-for map_nam in list(maps)[1:]:
+for map_nam in list(maps)[1:-1]:
     my_map = maps[map_nam]
 
     big_dict[map_nam] = make_map_dict(my_map)
 
 # print(big_dict)
 
-def find_locations(seeds,
-                  big_dict) -> list:
+def find_locations(seeds:list,
+                  big_dict:dict,
+                  seed_ranges:bool = False) -> list:
     locations = []
     for seed in seeds:
         output = seed # start with seed as the key
-        
+        # print(output)
         for now_map in list(big_dict.values()):
-            try:
-                output = now_map[output]
+            # loop through the tuples, if number >= than source[0] and < source[1]
+                # match it to destination by (number - source[0]) + destination[0]
+            found = False
+            for tuple_range in now_map:
+                if output >= tuple_range[0] and output < tuple_range[1]:
+                    # print(seed, output, tuple_range, now_map[tuple_range])
+                    output = (output - tuple_range[0]) + now_map[tuple_range][0]
+                    found = True
+                    break
+            
             # if source not in map, the destination is the same number
-            except KeyError:
-                output = output
+            else:
+                if found == False:
+                    output = output
+                
         # after we have traversed through all the maps
         else:
             locations.append(output)
@@ -86,4 +103,4 @@ def find_locations(seeds,
 locs = find_locations(maps['seeds'][0],
                       big_dict)
 
-print('Part1:', min(locs))
+print('Part1:', min(locs)) # 57075758
